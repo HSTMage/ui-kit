55102801123c443ba4151925f3b350db0a552013
Lubomir Panak <lubopanak@gmail.com>
2016-06-10 15:19:51 +0200

    range slider

--- js/utils.js
+++ js/utils.js
@@ -56,6 +56,17 @@ $(function ()
 
 		  });
 	  }
+
+	  $("#slider-range").slider({
+		                            range: true,
+		                            min: 0,
+		                            max: 500,
+		                            values: [75, 300],
+		                            slide: function (event, ui)
+		                            {
+		                            }
+	                            });
+
 	  init_graphs();
 
   });

504f00a5a0cc1d0bab8f705f900f62471ec6d055
Lubomir Panak <lubopanak@gmail.com>
2016-06-10 13:45:19 +0200

    graphs

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,43 @@
 function init_graphs()
 {
+	var data = {
+		series: [40, 10, 20, 30]
+	};
+	new Chartist.Pie('#pie', data);
+	new Chartist.Bar('#bar', {
+		labels: ['', '', '', ''],
+		series: [40, 10, 20, 30]
+	}, {
+		distributeSeries: true,
+		axisX: {
+			showLabel: false
+		},
+		axisY: {
+			showLabel: false
+		}
+
+	});
+	new Chartist.Bar('#bar2', {
+		labels: ['', '', '', ''],
+		series: [40, 10, 20, 30]
+	}, {
+		horizontalBars: true,
+		distributeSeries: true,
+		axisX: {
+			showLabel: false,
+			ticks: [0],
+			divisor: [100]
+		},
+		axisY: {
+			showLabel: false
+		}
+
+	});
 
 }
 $(function ()
   {
+	  var spinner = $("#spinner").spinner();
 
 	  if ($('.calendar-input').length)
 	  {
